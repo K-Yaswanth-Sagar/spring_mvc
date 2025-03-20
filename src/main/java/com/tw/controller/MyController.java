2db9fdd964b4aea4e130c2acc7a2b2b89a2a6a7c
K-Yaswanth-Sagar <yaswanthsagar0307@gmail.com>
2025-03-20 14:20:58 +0530

    implemented crud operations with Thymeleaf

--- src/main/java/com/tw/controller/MyController.java
+++ src/main/java/com/tw/controller/MyController.java
@@ -6,10 +6,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.tw.entity.FoodMenu;
 import com.tw.service.MenuService;
@@ -21,24 +22,53 @@ public class MyController {
 	@Autowired
 	private MenuService ms;
 	
-	@GetMapping("/index")
-    public String showMenuPage(Model model) {
+	@GetMapping("/food-menu")
+    public ModelAndView showMenuPage() {
+		ModelAndView mv = new ModelAndView();
         List<FoodMenu> menuList = ms.getMemu();
-        model.addAttribute("menu", menuList);
-        return "index";  
+        mv.addObject("menus", menuList);
+        mv.setViewName("food-menu");
+        return mv;
     }
 
-	@GetMapping("/addMenu")
-    public String showAddMenuForm(Model model) {
-        model.addAttribute("foodMenu", new FoodMenu()); 
-        return "add-menu"; 
+	@GetMapping("/add-menu")
+    public ModelAndView showAddMenuForm() {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("newItem", new FoodMenu());
+		mv.setViewName("add-menu");
+		return mv;
     }
 
-    @PostMapping("/addMenu")
-    public String save(@ModelAttribute FoodMenu fm) {
-        ms.saveMenu(fm);
-        return "redirect:/index"; 
+    @PostMapping("/add-menu")
+    public ModelAndView save(@ModelAttribute FoodMenu fm) {
+    	ms.saveMenu(fm);
+    	ModelAndView mv = new ModelAndView();
+        mv.setViewName("redirect:/add-menu");
+        return mv; 
     }
+    
+    @GetMapping("/edit-menu/{id}")
+    public ModelAndView showEditMenuForm(@PathVariable Long id) {
+        ModelAndView mv = new ModelAndView("edit-menu");
+        FoodMenu foodMenu = ms.getMenuById(id); 
+        mv.addObject("foodMenu", foodMenu);
+        return mv;
+    }
+
+    @PostMapping("/update-menu")
+    public String updateMenu(@ModelAttribute FoodMenu foodMenu) {
+        ms.updateMenu(foodMenu); 
+        
+        return "redirect:/food-menu"; 
+    }
+
+    @GetMapping("/delete-menu/{id}")
+    public String deleteMenu(@PathVariable Long id) {
+        ms.deleteMenu(id);
+        
+        return "redirect:/food-menu";
+    }
+
 	
 	
 	
--- src/main/java/com/tw/service/MenuService.java
+++ src/main/java/com/tw/service/MenuService.java
@@ -1,6 +1,7 @@
 package com.tw.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,5 +22,28 @@ public class MenuService {
 	public List<FoodMenu> getMemu(){
 		return repo.findAll(); 
 	}
+	
+	public FoodMenu getMenuById(Long id) {
+	    return repo.findById(id).orElse(null);
+	}
+	
+	public boolean updateMenu(FoodMenu foodMenu) {
+	    Optional<FoodMenu> existingMenu = repo.findById(foodMenu.getId());
+	    if (existingMenu.isPresent()) {
+	        repo.save(foodMenu); 
+	        return true;
+	    }
+	    return false;
+	}
+	
+	public boolean deleteMenu(Long id) {
+	    if (repo.existsById(id)) {
+	        repo.deleteById(id);
+	        return true;
+	    }
+	    return false;
+	}
+
+
 	
 }
